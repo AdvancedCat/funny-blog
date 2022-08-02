b3cb684169f17dc02b4727fedf2b2c521f5b7aec
advancedcat <wshx1938@163.com>
2022-08-02 20:04:57 +0800

    api middleware

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,25 @@
-export default function handler(req, res) {
-    const query = req.query
-    console.log('query:', query)
+import Cors from 'cors';
+
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+});
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+
+            return resolve(result);
+        });
+    });
+}
+
+export default async function handler(req, res) {
+    await runMiddleware(req, res, cors);
+
+    const query = req.query;
+    console.log('query:', query);
     res.status(200).json({ text: 'Hello ' + JSON.stringify(query) });
 }
