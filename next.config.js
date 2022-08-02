8485efa12239248ed332f7994d07d08e23bc8fce
advancedcat <wshx1938@163.com>
2022-08-02 20:25:13 +0800

    next export

--- next.config.js
+++ next.config.js
@@ -13,15 +13,24 @@ module.exports = {
             // },
         ];
     },
-    // add custom headers for all routes 
+    // add custom headers for all routes
     // 如果要针对请求级别添加头部，则在业务代码中添加
-    async headers(){
-        return [{
-            source: '/:path*',
-            headers: [{
-                key: 'X-DNS-Prefetch-Control',
-                value: 'on'
-            }]
-        }]
-    }
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on',
+                    },
+                ],
+            },
+        ];
+    },
+    experimental: {
+        images: {
+            unoptimized: true,
+        },
+    },
 };
